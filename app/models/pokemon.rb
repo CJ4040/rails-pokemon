c16a115f050b970f2475fb6a3080b4f1b114df13
Chris Cashin <cashin.cj@gmail.com>
2024-10-02 20:12:28 +0200

    seed database

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,5 @@
 class Pokemon < ApplicationRecord
+  validates :name, presence: true
+  # validates :element_type, :inclusion => { :in => %w[Fire Water Earth Toxic] }
+  has_one_attached :photo
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,2 +1,10 @@
 class Trainer < ApplicationRecord
+  has_many :pokeballs
+  has_many :pokemons, through: :pokeballs
+  validates :name, presence: true
+  validates :age, presence: true
+  has_one_attached :photo
+
+
+
 end
